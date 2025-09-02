ef6c0ca727ecf3c7bde5e0ef53c814fb3b45c0b4
Da <133000883+RexP10r@users.noreply.github.com>
2025-09-02 23:03:55 +0300

    [delete] extra directory

--- src/format.sh
+++
@@ -1,4 +0,0 @@
-clang-format -style=Google -i *.h funcs/* tests/* simple_tests/*
-clang-format -style=Google -n *.h funcs/* tests/* simple_tests/*
-cppcheck --enable=all --suppress=missingIncludeSystem *.c *.h funcs/* tests/* simple_tests/*
-
